158ced2f990ad597348beb1205d93d6032a7b01c
Paul Ofili <paulofili42@gmail.com>
2021-08-29 04:49:52 +0100

    Added health endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flask_cors import cross_origin
 
 from similarity_checker.similarity_checker_route import similarity_checker_bp
 from similarity_checker.module.copyleaks.commandfailederror import CommandFailedError
@@ -8,6 +9,11 @@ from similarity_checker.contollers.similarity_service import  initializeCopyleak
 load_dotenv()
 
 app = Flask(__name__)
+
+@app.route('/health', methods=["GET"])
+@cross_origin()
+def getHealth():
+   return "Ok"
 
 app.register_blueprint(similarity_checker_bp, url_prefix="/calculate-similarity")
 
